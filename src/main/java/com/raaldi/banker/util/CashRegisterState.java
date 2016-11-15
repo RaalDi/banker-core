43b95f91f0ff00cccff40d9bbc622878ed94a88b
Rafael Alexander Diaz <rafael.alexander.diaz@gmail.com>
2016-11-15 07:46:07 -0500

    Moved ENUMs

--- src/main/java/com/raaldi/banker/util/CashRegisterState.java
+++
@@ -1,5 +0,0 @@
-package com.raaldi.banker.util;
-
-public enum CashRegisterState {
-  OPENING, OPENED, CLOSING, CLOSED
-}
--- src/main/java/com/raaldi/banker/util/SessionState.java
+++
@@ -1,5 +0,0 @@
-package com.raaldi.banker.util;
-
-public enum SessionState {
-  STARTING, STARTED, ENDING, ENDED
-}
